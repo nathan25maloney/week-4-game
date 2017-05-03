13d0fc4de8d3456acd515e214e301d061b1d706b
Nathan Maloney <Nathan Maloney>
2017-05-02 21:20:31 -0700

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,3 @@
-
-
-
 var computerChoice = 0;
 var crystal1 = 0;
 var crystal2 = 0;
@@ -10,24 +7,34 @@ var scoreNumber = 0;
 var wins = 0;
 var loses = 0;
 
-
 function randomNumber(){
 	computerChoice = Math.floor(Math.random()*100) + 50;
 	
 }
-function reset(){
-	randomNumber();
-	crystal1 = 0;
-	crystal2 = 0;
-	crystal3 = 0;
-	crystal4 = 0;
-	$('#computerChoice').text(computerChoice);
-    scoreNumber = 0;
-    $('#counter').text(scoreNumber);
-}
-
-function crystalNumber(crystal){
-	crystal = Math.floor(Math.random()*20);
+function resetCheck(){
+	if(scoreNumber > computerChoice){
+        loses += 1;
+ 		randomNumber();
+ 		$('#loses').text(loses);
+ 		$('#computerChoice').text(computerChoice);
+ 		scoreNumber = 0;
+ 		$('#counter').text(scoreNumber);
+		crystal1 = 0;
+		crystal2 = 0;
+		crystal3 = 0;
+		crystal4 = 0;
+	} else if (scoreNumber === computerChoice){
+		wins += 1;
+		randomNumber();
+		$('#wins').text(wins);
+		$('#computerChoice').text(computerChoice);
+		scoreNumber = 0;
+		$('#counter').text(scoreNumber);
+		crystal1 = 0;
+		crystal2 = 0;
+		crystal3 = 0;
+		crystal4 = 0;
+            		}
 }
 
 $( window ).on( "load", randomNumber());
@@ -35,294 +42,73 @@ $( window ).on( "load", randomNumber());
 $(document).ready(function(){ 
 	$('#computerChoice').text(computerChoice);
 
-	
-
 	$("#button-1").on("click", function() {
             if (scoreNumber< computerChoice) {
             	if (crystal1 === 0) {
-
-            		crystal1 = Math.floor(Math.random()*20)+5;
+					crystal1 = Math.floor(Math.random()*20)+5;
             		console.log(crystal1);
             		scoreNumber = scoreNumber + crystal1;
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber > computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if (scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
-
-
+            		resetCheck();
             	} else {
             		scoreNumber = scoreNumber + crystal1;
-
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber> computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if(scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
+            		resetCheck();
             		console.log(crystal1);
             	}
-
-            } 
-            
+            }    
         });
 
     $("#button-2").on("click", function() {
     	if (scoreNumber< computerChoice) {
             	if (crystal2 === 0) {
-
             		crystal2 = Math.floor(Math.random()*20)+5;
             		console.log(crystal2);
             		scoreNumber = scoreNumber + crystal2;
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber > computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if (scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
-
-
+            		resetCheck();
             	} else {
             		scoreNumber = scoreNumber + crystal2;
-
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber > computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if(scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
+            		resetCheck();
             		console.log(crystal2);
             	}
-
-            } 
-            
-            
-            
+            }   
         });
 
 
     $("#button-3").on("click", function() {
     	if (scoreNumber< computerChoice) {
             	if (crystal3 === 0) {
-
             		crystal3 = Math.floor(Math.random()*20)+5;
             		console.log(crystal3);
             		scoreNumber = scoreNumber + crystal3;
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber > computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if (scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
-
-
+            		resetCheck();
             	} else {
             		scoreNumber = scoreNumber + crystal3;
-
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber> computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if(scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
+            		resetCheck();
             		console.log(crystal3);
             	}
-
-            } 
-            
-            
-            
+            }     
         });
 
 
     $("#button-4").on("click", function() {
     	if (scoreNumber< computerChoice) {
             	if (crystal4 === 0) {
-
             		crystal4 = Math.floor(Math.random()*20)+5;
             		console.log(crystal4);
             		scoreNumber = scoreNumber + crystal4;
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber > computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if (scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
-
-
+            		resetCheck();
             	} else {
             		scoreNumber = scoreNumber + crystal4;
-
             		$('#counter').text(scoreNumber);
-
-            		if(scoreNumber> computerChoice){
-            			loses += 1;
-            			randomNumber();
-            			$('#loses').text(loses);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		} else if(scoreNumber === computerChoice){
-            			wins += 1;
-            			randomNumber();
-            			$('#wins').text(wins);
-            			$('#computerChoice').text(computerChoice);
-            			scoreNumber = 0;
-            			$('#counter').text(scoreNumber);
-            			crystal1 = 0;
-            			crystal2 = 0;
-            			crystal3 = 0;
-            			crystal4 = 0;
-            		}
+            		resetCheck();
             		console.log(crystal4);
             	}
-
             } 
-            
-            
-            
         });
-
-	
-
-
-
 });
